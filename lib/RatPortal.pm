789deaef8cb1a1a9e9cc067e674eafd6cc6764ce
Alex Kalderimis <alex@flymine.org>
2011-10-14 15:28:45 +0100

    Added boilerpate

--- lib/RatPortal.pm
+++ lib/RatPortal.pm
@@ -3,6 +3,10 @@ use Dancer ':syntax';
 
 our $VERSION = '0.1';
 
+use Webservice::InterMine;
+
+my $ratmine = get_service("development.ratmine.mcw.edu/ratmine", 'ratportal@mcw.edu', 'test');
+
 get '/' => sub {
     template 'index';
 };
